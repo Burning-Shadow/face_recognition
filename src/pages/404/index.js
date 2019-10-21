667aba7a5dbd3c62ee81b8ca58626530776b58e5
burning-shadow <2485999859@qq.com>
2019-10-21 20:44:05 +0800

    add route

---
+++ src/pages/404/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default () => (
+    <div>
+      Sorry, we have no such Page which you input, plz check it.
+    </div>
+  )
--- src/router.js
+++ src/router.js
@@ -1,24 +1,35 @@
 import React from "react";
-import Main from "./route/main";
-import About from "./route/about";
-import Topic from "./route/topic";
-import Home from "./route/home";
-import { HashRouter as Router, Route } from "react-router-dom";
+import App from "./App";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+// import Login from './'
+// import Main from "./route/main";
+// import About from "./route/about";
+// import Topic from "./route/topic";
+// import Home from "./route/home";
 
 // import Home from "./route/home"
+import Login from "./pages/login";
+import Admin from "./pages/admin";
+import NoneMatch from "./pages/404";
 
 export default class IRouter extends React.Component {
   render() {
     return (
       <Router>
-        <Home>
-          <Route path="/main" component={Main}></Route>
-          <Route path="/about" component={About}></Route>
-          <Route path="/topics" component={Topic}></Route>
-        </Home>
+        <App>
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/admin" component={Admin} />
+            <Route component={NoneMatch} />
+          </Switch>
+        </App>
+        {/* <Home>
+          <Route path="/main" component={Main} />
+          <Route path="/about" component={About} />
+          <Route path="/topics" component={Topic} />
+        </Home> */}
       </Router>
     );
   }
 }
-
-
